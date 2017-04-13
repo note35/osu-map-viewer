03bbe9680d6eb7162f55a89ab77aa52714328a40
Kir Chou <note351@hotmail.com>
2017-04-13 17:13:22 +0800

    multi-routing

--- viewer/src/js/containers/index.js
+++ viewer/src/js/containers/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect, Link } from 'react-router-dom'
+import { Router, Route, Redirect, Link } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import Map from './Map';
 
+const history = createBrowserHistory()
+
 const routes = (
-  <BrowserRouter>
-    <Route path="/" component={Map}/>
-  </BrowserRouter>
+  <Router history={history}>
+    <div>
+      <Redirect from="/" to="/map" />
+      <Route path="/map" component={Map}/>
+    </div>
+  </Router>
 );
 
 export default routes;
